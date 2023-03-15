ca44edac7dccc25ee9a0bca0156031dc67f43184
William Wong <william.wai.lim.wong@gmail.com>
2023-03-15 08:52:56 -0400

    completed Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42-someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start-destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance<=400){
+        return 0;
+    } else if (distance<=2000){
+        return (distance-400)*0.02;
+    } else if (distance<=2500){
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
